b7470a7daf2ffd37cbb41e8388dfcae526472ba0
Olaf Bernstein <camel-cdr@protonmail.com>
2022-07-11 00:41:50 +0200

    added cm.c

---
+++ boline/cm.c
@@ -0,0 +1,270 @@
+#define CM_SCAN(...) __VA_ARGS__
+#define CM_EAT(...)
+#define CM_LPAREN (
+
+#define CM_00(...)  CM_SCAN(CM_EAT CM_LPAREN CM__00(__VA_ARGS__))
+#define CM_01(...)  CM_SCAN(CM_EAT CM_LPAREN CM__01(__VA_ARGS__))
+#define CM_02(...)  CM_SCAN(CM_EAT CM_LPAREN CM__02(__VA_ARGS__))
+#define CM_03(...)  CM_SCAN(CM_EAT CM_LPAREN CM__03(__VA_ARGS__))
+#define CM_04(...)  CM_SCAN(CM_EAT CM_LPAREN CM__04(__VA_ARGS__))
+#define CM_05(...)  CM_SCAN(CM_EAT CM_LPAREN CM__05(__VA_ARGS__))
+#define CM_06(...)  CM_SCAN(CM_EAT CM_LPAREN CM__06(__VA_ARGS__))
+#define CM_07(...)  CM_SCAN(CM_EAT CM_LPAREN CM__07(__VA_ARGS__))
+#define CM_08(...)  CM_SCAN(CM_EAT CM_LPAREN CM__08(__VA_ARGS__))
+#define CM_09(...)  CM_SCAN(CM_EAT CM_LPAREN CM__09(__VA_ARGS__))
+#define CM_0a(...)  CM_SCAN(CM_EAT CM_LPAREN CM__0a(__VA_ARGS__))
+#define CM_0b(...)  CM_SCAN(CM_EAT CM_LPAREN CM__0b(__VA_ARGS__))
+#define CM_0c(...)  CM_SCAN(CM_EAT CM_LPAREN CM__0c(__VA_ARGS__))
+#define CM_0d(...)  CM_SCAN(CM_EAT CM_LPAREN CM__0d(__VA_ARGS__))
+#define CM_0e(...)  CM_SCAN(CM_EAT CM_LPAREN CM__0e(__VA_ARGS__))
+#define CM_0f(...)  CM_SCAN(CM_EAT CM_LPAREN CM__0f(__VA_ARGS__))
+#define CM_10(...)  CM_SCAN(CM_EAT CM_LPAREN CM__10(__VA_ARGS__))
+#define CM_11(...)  CM_SCAN(CM_EAT CM_LPAREN CM__11(__VA_ARGS__))
+#define CM_12(...)  CM_SCAN(CM_EAT CM_LPAREN CM__12(__VA_ARGS__))
+#define CM_13(...)  CM_SCAN(CM_EAT CM_LPAREN CM__13(__VA_ARGS__))
+#define CM_14(...)  CM_SCAN(CM_EAT CM_LPAREN CM__14(__VA_ARGS__))
+#define CM_15(...)  CM_SCAN(CM_EAT CM_LPAREN CM__15(__VA_ARGS__))
+#define CM_16(...)  CM_SCAN(CM_EAT CM_LPAREN CM__16(__VA_ARGS__))
+#define CM_17(...)  CM_SCAN(CM_EAT CM_LPAREN CM__17(__VA_ARGS__))
+#define CM_18(...)  CM_SCAN(CM_EAT CM_LPAREN CM__18(__VA_ARGS__))
+#define CM_19(...)  CM_SCAN(CM_EAT CM_LPAREN CM__19(__VA_ARGS__))
+#define CM_1a(...)  CM_SCAN(CM_EAT CM_LPAREN CM__1a(__VA_ARGS__))
+#define CM_1b(...)  CM_SCAN(CM_EAT CM_LPAREN CM__1b(__VA_ARGS__))
+#define CM_1c(...)  CM_SCAN(CM_EAT CM_LPAREN CM__1c(__VA_ARGS__))
+#define CM_1d(...)  CM_SCAN(CM_EAT CM_LPAREN CM__1d(__VA_ARGS__))
+#define CM_1e(...)  CM_SCAN(CM_EAT CM_LPAREN CM__1e(__VA_ARGS__))
+#define CM_1f(...)  CM_SCAN(CM_EAT CM_LPAREN CM__1f(__VA_ARGS__))
+#define CM_20(...)  CM_SCAN(CM_EAT CM_LPAREN CM__20(__VA_ARGS__))
+#define CM_21(...)  CM_SCAN(CM_EAT CM_LPAREN CM__21(__VA_ARGS__))
+#define CM_22(...)  CM_SCAN(CM_EAT CM_LPAREN CM__22(__VA_ARGS__))
+#define CM_23(...)  CM_SCAN(CM_EAT CM_LPAREN CM__23(__VA_ARGS__))
+#define CM_24(...)  CM_SCAN(CM_EAT CM_LPAREN CM__24(__VA_ARGS__))
+#define CM_25(...)  CM_SCAN(CM_EAT CM_LPAREN CM__25(__VA_ARGS__))
+#define CM_26(...)  CM_SCAN(CM_EAT CM_LPAREN CM__26(__VA_ARGS__))
+#define CM_27(...)  CM_SCAN(CM_EAT CM_LPAREN CM__27(__VA_ARGS__))
+#define CM_28(...)  CM_SCAN(CM_EAT CM_LPAREN CM__28(__VA_ARGS__))
+#define CM_29(...)  CM_SCAN(CM_EAT CM_LPAREN CM__29(__VA_ARGS__))
+#define CM_2a(...)  CM_SCAN(CM_EAT CM_LPAREN CM__2a(__VA_ARGS__))
+#define CM_2b(...)  CM_SCAN(CM_EAT CM_LPAREN CM__2b(__VA_ARGS__))
+#define CM_2c(...)  CM_SCAN(CM_EAT CM_LPAREN CM__2c(__VA_ARGS__))
+#define CM_2d(...)  CM_SCAN(CM_EAT CM_LPAREN CM__2d(__VA_ARGS__))
+#define CM_2e(...)  CM_SCAN(CM_EAT CM_LPAREN CM__2e(__VA_ARGS__))
+#define CM_2f(...)  CM_SCAN(CM_EAT CM_LPAREN CM__2f(__VA_ARGS__))
+#define CM_30(...)  CM_SCAN(CM_EAT CM_LPAREN CM__30(__VA_ARGS__))
+#define CM_31(...)  CM_SCAN(CM_EAT CM_LPAREN CM__31(__VA_ARGS__))
+#define CM_32(...)  CM_SCAN(CM_EAT CM_LPAREN CM__32(__VA_ARGS__))
+#define CM_33(...)  CM_SCAN(CM_EAT CM_LPAREN CM__33(__VA_ARGS__))
+#define CM_34(...)  CM_SCAN(CM_EAT CM_LPAREN CM__34(__VA_ARGS__))
+#define CM_35(...)  CM_SCAN(CM_EAT CM_LPAREN CM__35(__VA_ARGS__))
+#define CM_36(...)  CM_SCAN(CM_EAT CM_LPAREN CM__36(__VA_ARGS__))
+#define CM_37(...)  CM_SCAN(CM_EAT CM_LPAREN CM__37(__VA_ARGS__))
+#define CM_38(...)  CM_SCAN(CM_EAT CM_LPAREN CM__38(__VA_ARGS__))
+#define CM_39(...)  CM_SCAN(CM_EAT CM_LPAREN CM__39(__VA_ARGS__))
+#define CM_3a(...)  CM_SCAN(CM_EAT CM_LPAREN CM__3a(__VA_ARGS__))
+#define CM_3b(...)  CM_SCAN(CM_EAT CM_LPAREN CM__3b(__VA_ARGS__))
+#define CM_3c(...)  CM_SCAN(CM_EAT CM_LPAREN CM__3c(__VA_ARGS__))
+#define CM_3d(...)  CM_SCAN(CM_EAT CM_LPAREN CM__3d(__VA_ARGS__))
+#define CM_3e(...)  CM_SCAN(CM_EAT CM_LPAREN CM__3e(__VA_ARGS__))
+#define CM_3f(...)  CM_SCAN(CM_EAT CM_LPAREN CM__3f(__VA_ARGS__))
+
+#define CM__00(P,f,...) CM_P01(CM_P01(CM_##f(,P##__VA_ARGS__)))
+#define CM__01(P,f,...) CM_P02(CM_P02(CM_##f(,P##__VA_ARGS__)))
+#define CM__02(P,f,...) CM_P03(CM_P03(CM_##f(,P##__VA_ARGS__)))
+#define CM__03(P,f,...) CM_P04(CM_P04(CM_##f(,P##__VA_ARGS__)))
+#define CM__04(P,f,...) CM_P05(CM_P05(CM_##f(,P##__VA_ARGS__)))
+#define CM__05(P,f,...) CM_P06(CM_P06(CM_##f(,P##__VA_ARGS__)))
+#define CM__06(P,f,...) CM_P07(CM_P07(CM_##f(,P##__VA_ARGS__)))
+#define CM__07(P,f,...) CM_P08(CM_P08(CM_##f(,P##__VA_ARGS__)))
+#define CM__08(P,f,...) CM_P09(CM_P09(CM_##f(,P##__VA_ARGS__)))
+#define CM__09(P,f,...) CM_P0a(CM_P0a(CM_##f(,P##__VA_ARGS__)))
+#define CM__0a(P,f,...) CM_P0b(CM_P0b(CM_##f(,P##__VA_ARGS__)))
+#define CM__0b(P,f,...) CM_P0c(CM_P0c(CM_##f(,P##__VA_ARGS__)))
+#define CM__0c(P,f,...) CM_P0d(CM_P0d(CM_##f(,P##__VA_ARGS__)))
+#define CM__0d(P,f,...) CM_P0e(CM_P0e(CM_##f(,P##__VA_ARGS__)))
+#define CM__0e(P,f,...) CM_P0f(CM_P0f(CM_##f(,P##__VA_ARGS__)))
+#define CM__0f(P,f,...) CM_P10(CM_P10(CM_##f(,P##__VA_ARGS__)))
+#define CM__10(P,f,...) CM_P11(CM_P11(CM_##f(,P##__VA_ARGS__)))
+#define CM__11(P,f,...) CM_P12(CM_P12(CM_##f(,P##__VA_ARGS__)))
+#define CM__12(P,f,...) CM_P13(CM_P13(CM_##f(,P##__VA_ARGS__)))
+#define CM__13(P,f,...) CM_P14(CM_P14(CM_##f(,P##__VA_ARGS__)))
+#define CM__14(P,f,...) CM_P15(CM_P15(CM_##f(,P##__VA_ARGS__)))
+#define CM__15(P,f,...) CM_P16(CM_P16(CM_##f(,P##__VA_ARGS__)))
+#define CM__16(P,f,...) CM_P17(CM_P17(CM_##f(,P##__VA_ARGS__)))
+#define CM__17(P,f,...) CM_P18(CM_P18(CM_##f(,P##__VA_ARGS__)))
+#define CM__18(P,f,...) CM_P19(CM_P19(CM_##f(,P##__VA_ARGS__)))
+#define CM__19(P,f,...) CM_P1a(CM_P1a(CM_##f(,P##__VA_ARGS__)))
+#define CM__1a(P,f,...) CM_P1b(CM_P1b(CM_##f(,P##__VA_ARGS__)))
+#define CM__1b(P,f,...) CM_P1c(CM_P1c(CM_##f(,P##__VA_ARGS__)))
+#define CM__1c(P,f,...) CM_P1d(CM_P1d(CM_##f(,P##__VA_ARGS__)))
+#define CM__1d(P,f,...) CM_P1e(CM_P1e(CM_##f(,P##__VA_ARGS__)))
+#define CM__1e(P,f,...) CM_P1f(CM_P1f(CM_##f(,P##__VA_ARGS__)))
+#define CM__1f(P,f,...) CM_P20(CM_P20(CM_##f(,P##__VA_ARGS__)))
+#define CM__20(P,f,...) CM_P21(CM_P21(CM_##f(,P##__VA_ARGS__)))
+#define CM__21(P,f,...) CM_P22(CM_P22(CM_##f(,P##__VA_ARGS__)))
+#define CM__22(P,f,...) CM_P23(CM_P23(CM_##f(,P##__VA_ARGS__)))
+#define CM__23(P,f,...) CM_P24(CM_P24(CM_##f(,P##__VA_ARGS__)))
+#define CM__24(P,f,...) CM_P25(CM_P25(CM_##f(,P##__VA_ARGS__)))
+#define CM__25(P,f,...) CM_P26(CM_P26(CM_##f(,P##__VA_ARGS__)))
+#define CM__26(P,f,...) CM_P27(CM_P27(CM_##f(,P##__VA_ARGS__)))
+#define CM__27(P,f,...) CM_P28(CM_P28(CM_##f(,P##__VA_ARGS__)))
+#define CM__28(P,f,...) CM_P29(CM_P29(CM_##f(,P##__VA_ARGS__)))
+#define CM__29(P,f,...) CM_P2a(CM_P2a(CM_##f(,P##__VA_ARGS__)))
+#define CM__2a(P,f,...) CM_P2b(CM_P2b(CM_##f(,P##__VA_ARGS__)))
+#define CM__2b(P,f,...) CM_P2c(CM_P2c(CM_##f(,P##__VA_ARGS__)))
+#define CM__2c(P,f,...) CM_P2d(CM_P2d(CM_##f(,P##__VA_ARGS__)))
+#define CM__2d(P,f,...) CM_P2e(CM_P2e(CM_##f(,P##__VA_ARGS__)))
+#define CM__2e(P,f,...) CM_P2f(CM_P2f(CM_##f(,P##__VA_ARGS__)))
+#define CM__2f(P,f,...) CM_P30(CM_P30(CM_##f(,P##__VA_ARGS__)))
+#define CM__30(P,f,...) CM_P31(CM_P31(CM_##f(,P##__VA_ARGS__)))
+#define CM__31(P,f,...) CM_P32(CM_P32(CM_##f(,P##__VA_ARGS__)))
+#define CM__32(P,f,...) CM_P33(CM_P33(CM_##f(,P##__VA_ARGS__)))
+#define CM__33(P,f,...) CM_P34(CM_P34(CM_##f(,P##__VA_ARGS__)))
+#define CM__34(P,f,...) CM_P35(CM_P35(CM_##f(,P##__VA_ARGS__)))
+#define CM__35(P,f,...) CM_P36(CM_P36(CM_##f(,P##__VA_ARGS__)))
+#define CM__36(P,f,...) CM_P37(CM_P37(CM_##f(,P##__VA_ARGS__)))
+#define CM__37(P,f,...) CM_P38(CM_P38(CM_##f(,P##__VA_ARGS__)))
+#define CM__38(P,f,...) CM_P39(CM_P39(CM_##f(,P##__VA_ARGS__)))
+#define CM__39(P,f,...) CM_P3a(CM_P3a(CM_##f(,P##__VA_ARGS__)))
+#define CM__3a(P,f,...) CM_P3b(CM_P3b(CM_##f(,P##__VA_ARGS__)))
+#define CM__3b(P,f,...) CM_P3c(CM_P3c(CM_##f(,P##__VA_ARGS__)))
+#define CM__3c(P,f,...) CM_P3d(CM_P3d(CM_##f(,P##__VA_ARGS__)))
+#define CM__3d(P,f,...) CM_P3e(CM_P3e(CM_##f(,P##__VA_ARGS__)))
+#define CM__3e(P,f,...) CM_P3f(CM_P3f(CM_##f(,P##__VA_ARGS__)))
+#define CM__3f(P,f,...) ERROR
+
+#define CM_P01(x) CM__01 x
+#define CM_P02(x) CM__02 x
+#define CM_P03(x) CM__03 x
+#define CM_P04(x) CM__04 x
+#define CM_P05(x) CM__05 x
+#define CM_P06(x) CM__06 x
+#define CM_P07(x) CM__07 x
+#define CM_P08(x) CM__08 x
+#define CM_P09(x) CM__09 x
+#define CM_P0a(x) CM__0a x
+#define CM_P0b(x) CM__0b x
+#define CM_P0c(x) CM__0c x
+#define CM_P0d(x) CM__0d x
+#define CM_P0e(x) CM__0e x
+#define CM_P0f(x) CM__0f x
+#define CM_P10(x) CM__10 x
+#define CM_P11(x) CM__11 x
+#define CM_P12(x) CM__12 x
+#define CM_P13(x) CM__13 x
+#define CM_P14(x) CM__14 x
+#define CM_P15(x) CM__15 x
+#define CM_P16(x) CM__16 x
+#define CM_P17(x) CM__17 x
+#define CM_P18(x) CM__18 x
+#define CM_P19(x) CM__19 x
+#define CM_P1a(x) CM__1a x
+#define CM_P1b(x) CM__1b x
+#define CM_P1c(x) CM__1c x
+#define CM_P1d(x) CM__1d x
+#define CM_P1e(x) CM__1e x
+#define CM_P1f(x) CM__1f x
+#define CM_P21(x) CM__21 x
+#define CM_P22(x) CM__22 x
+#define CM_P23(x) CM__23 x
+#define CM_P24(x) CM__24 x
+#define CM_P25(x) CM__25 x
+#define CM_P26(x) CM__26 x
+#define CM_P27(x) CM__27 x
+#define CM_P28(x) CM__28 x
+#define CM_P29(x) CM__29 x
+#define CM_P2a(x) CM__2a x
+#define CM_P2b(x) CM__2b x
+#define CM_P2c(x) CM__2c x
+#define CM_P2d(x) CM__2d x
+#define CM_P2e(x) CM__2e x
+#define CM_P2f(x) CM__2f x
+#define CM_P30(x) CM__30 x
+#define CM_P31(x) CM__31 x
+#define CM_P32(x) CM__32 x
+#define CM_P33(x) CM__33 x
+#define CM_P34(x) CM__34 x
+#define CM_P35(x) CM__35 x
+#define CM_P36(x) CM__36 x
+#define CM_P37(x) CM__37 x
+#define CM_P38(x) CM__38 x
+#define CM_P39(x) CM__39 x
+#define CM_P3a(x) CM__3a x
+#define CM_P3b(x) CM__3b x
+#define CM_P3c(x) CM__3c x
+#define CM_P3d(x) CM__3d x
+#define CM_P3e(x) CM__3e x
+#define CM_P3f(x) CM__3f x
+
+#define CM_ERROR_NO_MORE_MACHINE_STNE_ATE()
+
+#define CM_PROBE ),1,(
+#define CM_IF(x) CM_IF_(x,0,)
+#define CM_IF_(b,a,...) CM_IF_##a
+#define CM_IF_1(a,b) a
+#define CM_IF_0(a,b) b
+
+#define CM \
+	CM_IF(CM__1f(CM_,,PROBE))( \
+	    CM_IF(CM__0f(CM_,,PROBE))( \
+	        CM_IF(CM__07(CM_,,PROBE))( \
+	              CM_IF(CM__03(CM_,,PROBE))( \
+	                    CM_IF(CM__01(CM_,,PROBE))( \
+	                          CM_IF(CM__00(CM_,,PROBE))(CM_00,CM_01), \
+	                          CM_IF(CM__02(CM_,,PROBE))(CM_02,CM_03)), \
+	                    CM_IF(CM__05(CM_,,PROBE))( \
+	                          CM_IF(CM__04(CM_,,PROBE))(CM_04,CM_05), \
+	                          CM_IF(CM__06(CM_,,PROBE))(CM_06,CM_07))), \
+	              CM_IF(CM__0b(CM_,,PROBE))( \
+	                    CM_IF(CM__09(CM_,,PROBE))( \
+	                          CM_IF(CM__08(CM_,,PROBE))(CM_08,CM_09), \
+	                          CM_IF(CM__0a(CM_,,PROBE))(CM_0a,CM_0b)), \
+	                    CM_IF(CM__0d(CM_,,PROBE))( \
+	                          CM_IF(CM__0c(CM_,,PROBE))(CM_0c,CM_0d), \
+	                          CM_IF(CM__0e(CM_,,PROBE))(CM_0e,CM_0f)))), \
+	        CM_IF(CM__17(CM_,,PROBE))( \
+	              CM_IF(CM__13(CM_,,PROBE))( \
+	                    CM_IF(CM__11(CM_,,PROBE))( \
+	                          CM_IF(CM__10(CM_,,PROBE))(CM_10,CM_11), \
+	                          CM_IF(CM__12(CM_,,PROBE))(CM_12,CM_13)), \
+	                    CM_IF(CM__15(CM_,,PROBE))( \
+	                          CM_IF(CM__14(CM_,,PROBE))(CM_14,CM_15), \
+	                          CM_IF(CM__16(CM_,,PROBE))(CM_16,CM_17))), \
+	              CM_IF(CM__1b(CM_,,PROBE))( \
+	                    CM_IF(CM__19(CM_,,PROBE))( \
+	                          CM_IF(CM__18(CM_,,PROBE))(CM_18,CM_19), \
+	                          CM_IF(CM__1a(CM_,,PROBE))(CM_1a,CM_1b)), \
+	                    CM_IF(CM__1d(CM_,,PROBE))( \
+	                          CM_IF(CM__1c(CM_,,PROBE))(CM_1c,CM_1d), \
+	                          CM_IF(CM__1e(CM_,,PROBE))(CM_1e,CM_1f))))), \
+	    CM_IF(CM__2f(CM_,,PROBE))( \
+	        CM_IF(CM__27(CM_,,PROBE))( \
+	              CM_IF(CM__23(CM_,,PROBE))( \
+	                    CM_IF(CM__21(CM_,,PROBE))( \
+	                          CM_IF(CM__20(CM_,,PROBE))(CM_20,CM_21), \
+	                          CM_IF(CM__22(CM_,,PROBE))(CM_22,CM_23)), \
+	                    CM_IF(CM__25(CM_,,PROBE))( \
+	                          CM_IF(CM__24(CM_,,PROBE))(CM_24,CM_25), \
+	                          CM_IF(CM__26(CM_,,PROBE))(CM_26,CM_27))), \
+	              CM_IF(CM__2b(CM_,,PROBE))( \
+	                    CM_IF(CM__29(CM_,,PROBE))( \
+	                          CM_IF(CM__28(CM_,,PROBE))(CM_28,CM_29), \
+	                          CM_IF(CM__2a(CM_,,PROBE))(CM_2a,CM_2b)), \
+	                    CM_IF(CM__2d(CM_,,PROBE))( \
+	                          CM_IF(CM__2c(CM_,,PROBE))(CM_2c,CM_2d), \
+	                          CM_IF(CM__2e(CM_,,PROBE))(CM_2e,CM_2f)))), \
+	        CM_IF(CM__37(CM_,,PROBE))( \
+	              CM_IF(CM__33(CM_,,PROBE))( \
+	                    CM_IF(CM__31(CM_,,PROBE))( \
+	                          CM_IF(CM__30(CM_,,PROBE))(CM_30,CM_31), \
+	                          CM_IF(CM__32(CM_,,PROBE))(CM_32,CM_33)), \
+	                    CM_IF(CM__35(CM_,,PROBE))( \
+	                          CM_IF(CM__34(CM_,,PROBE))(CM_34,CM_35), \
+	                          CM_IF(CM__36(CM_,,PROBE))(CM_36,CM_37))), \
+	              CM_IF(CM__3b(CM_,,PROBE))( \
+	                    CM_IF(CM__39(CM_,,PROBE))( \
+	                          CM_IF(CM__38(CM_,,PROBE))(CM_38,CM_39), \
+	                          CM_IF(CM__3a(CM_,,PROBE))(CM_3a,CM_3b)), \
+	                    CM_IF(CM__3d(CM_,,PROBE))( \
+	                          CM_IF(CM__3c(CM_,,PROBE))(CM_3c,CM_3d), \
+	                          CM_IF(CM__3e(CM_,,PROBE))(CM_3e,CM_3f))))))
+
